8fbaf061a81f49716191303bb0fc643d16c383c9
Soovin Choi <58690808+AREACODE831@users.noreply.github.com>
2021-10-23 13:30:48 -0700

    updated scanning part

--- sketch2.js
+++ sketch2.js
@@ -1,17 +1,29 @@
 let cnv;
+let img;
+
+function preload(){
+  img = loadImage('img/shamz.png');
+}
 
 function setup() {
-  createCanvas(400, 400);
+  let cnv = createCanvas(400, 400);
+  image(img, 0,0);
+  cnv.position(windowWidth/3.5);
 }
 
 
 
 function draw() {
   background(0, 0, 0);
+  image(img, 150, 175, 150, 220);
   rect(0, mouseY, 400, 10);
   fill(255,0,0);
-   if (300 < mouseX && mouseY > 300) {
+   if (300 < mouseX && mouseY > 250) {
     fill(0,255,0);
+    textSize(20);
+    fill(0, 255, 0);
+    textFont('Orbitron');
+    text('PASS', 170, 80);
   }
 
 
