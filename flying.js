e65f4de2c25e7176adc3c044b4fc8b72b143b1c2
Soovin Choi <58690808+AREACODE831@users.noreply.github.com>
2021-10-25 08:51:39 -0700

    stylizing

---
+++ flying.js
@@ -0,0 +1,45 @@
+console.log("This is airport.js");
+
+var animation = anime.timeline({
+    autoplay: true
+});
+
+animation
+// .add({
+//     targets: '#btn',
+//     top: '1500px',
+//     duration: 500,
+//     easing: 'easeInOutSine'
+// })
+// .add({
+//   targets: '#btn',
+//   top: '50%',
+//   duration: 1000,
+//   easing: 'easeInOutBack'
+//        })
+
+
+.add({
+  targets: '#artist',
+  top: '0px',
+  duration: 1000,
+  easing: 'easeInOutSine'
+})
+
+
+.add({
+    targets: '#text1',
+    marginRight: 0,
+    duration: 1000,
+    easing: 'easeInOutBack'
+})
+
+.add({
+    targets: '#btn6',
+    marginRight: -300,
+    duration: 1000,
+    easing: 'easeInOutBack'
+})
+
+
+ document.querySelector('#btn').onclick = animation.play;
